53df58c973d395bdd0dd637b19a53f6186b9567c
Marc Wittke <marc@wittke-web.de>
2023-12-28 17:22:48 +0100

    packaging fix

--- src/Backend.Fx.AspNet.Feature/Mvc/AspNetMvcModule.cs
+++ src/Backend.Fx.AspNet.Feature/Mvc/AspNetMvcModule.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using Backend.Fx.Execution.DependencyInjection;
 using Backend.Fx.Util;
--- src/Backend.Fx.AspNet.Feature/SignalR/AspNetSignalRModule.cs
+++ src/Backend.Fx.AspNet.Feature/SignalR/AspNetSignalRModule.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using Backend.Fx.Execution.DependencyInjection;
