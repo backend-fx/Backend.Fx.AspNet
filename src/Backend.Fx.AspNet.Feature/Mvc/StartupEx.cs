cc1b502e2518f745cf0323eaf994ae4ae2459e5e
Marc Wittke <marc@wittke-web.de>
2024-03-23 17:30:40 +0100

    consider app state on execution

--- src/Backend.Fx.AspNet.Feature/Mvc/StartupEx.cs
+++ src/Backend.Fx.AspNet.Feature/Mvc/StartupEx.cs
@@ -18,28 +18,17 @@ public static class StartupEx
 
     public static void UseBackendFxMvcApplication(this IApplicationBuilder app, IBackendFxApplication application)
     {
+        app.UseWaitForBootMiddleware();
+        
         app.Use(async (context, requestDelegate) =>
         {
-            // make sure it finished the boot process
-            await application.WaitForBootAsync(context.RequestAborted).ConfigureAwait(false);
-
-            if (application.State == BackendFxApplicationState.BootFailed)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
-                await context.Response.WriteAsync(
-                    "Service Unavailable - please try again later",
-                    cancellationToken: context.RequestAborted);
-            }
-            else
-            {
-                await application
-                    .Invoker
-                    .InvokeAsync(
-                        (_, _) => requestDelegate.Invoke(),
-                        context.User.Identity ?? new AnonymousIdentity(),
-                        context.RequestAborted)
-                    .ConfigureAwait(false);
-            }
+            await application
+                .Invoker
+                .InvokeAsync(
+                    (_, _) => requestDelegate.Invoke(),
+                    context.User.Identity ?? new AnonymousIdentity(),
+                    context.RequestAborted)
+                .ConfigureAwait(false);
         });
     }
 }
